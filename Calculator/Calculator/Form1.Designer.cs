d953cedfd680afe2fcb79a2a26db3e927715a15f
Yasin Emre Karabacak <yasinemre453@gmail.com>
2024-10-29 14:00:43 +0300

    Add project files.

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Calculator
+{
+    partial class Calculatorlabel1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Calculatorlabel2 = new System.Windows.Forms.Label();
+            this.Calculatorlabel3 = new System.Windows.Forms.Label();
+            this.Calculatorentry1 = new System.Windows.Forms.TextBox();
+            this.Calculatorlabel4 = new System.Windows.Forms.Label();
+            this.Calculatorentry2 = new System.Windows.Forms.TextBox();
+            this.Calculatorbutton1 = new System.Windows.Forms.Button();
+            this.Calculatorbutton2 = new System.Windows.Forms.Button();
+            this.Calculatorbuton3 = new System.Windows.Forms.Button();
+            this.Calculatorbutton4 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Calculatorlabel5 = new System.Windows.Forms.Label();
+            this.Calculatorentry3 = new System.Windows.Forms.TextBox();
+            this.Calculatorbutton5 = new System.Windows.Forms.Button();
+            this.Calculatorlabel6 = new System.Windows.Forms.Label();
+            this.Calculatorbutton6 = new System.Windows.Forms.Button();
+            this.Calculatorbutton7 = new System.Windows.Forms.Button();
+            this.Calculatorbutton8 = new System.Windows.Forms.Button();
+            this.Calculatorbutton9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Calculatorlabel2
+            // 
+            this.Calculatorlabel2.AutoSize = true;
+            this.Calculatorlabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorlabel2.Location = new System.Drawing.Point(339, 21);
+            this.Calculatorlabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Calculatorlabel2.Name = "Calculatorlabel2";
+            this.Calculatorlabel2.Size = new System.Drawing.Size(351, 46);
+            this.Calculatorlabel2.TabIndex = 0;
+            this.Calculatorlabel2.Text = "Simple Calculator";
+            // 
+            // Calculatorlabel3
+            // 
+            this.Calculatorlabel3.AutoSize = true;
+            this.Calculatorlabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorlabel3.Location = new System.Drawing.Point(71, 132);
+            this.Calculatorlabel3.Name = "Calculatorlabel3";
+            this.Calculatorlabel3.Size = new System.Drawing.Size(123, 25);
+            this.Calculatorlabel3.TabIndex = 1;
+            this.Calculatorlabel3.Text = "First Number";
+            this.Calculatorlabel3.Click += new System.EventHandler(this.Calculatorlabel3_Click);
+            // 
+            // Calculatorentry1
+            // 
+            this.Calculatorentry1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorentry1.Location = new System.Drawing.Point(63, 172);
+            this.Calculatorentry1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorentry1.Multiline = true;
+            this.Calculatorentry1.Name = "Calculatorentry1";
+            this.Calculatorentry1.Size = new System.Drawing.Size(161, 80);
+            this.Calculatorentry1.TabIndex = 2;
+            // 
+            // Calculatorlabel4
+            // 
+            this.Calculatorlabel4.AutoSize = true;
+            this.Calculatorlabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorlabel4.Location = new System.Drawing.Point(487, 132);
+            this.Calculatorlabel4.Name = "Calculatorlabel4";
+            this.Calculatorlabel4.Size = new System.Drawing.Size(154, 25);
+            this.Calculatorlabel4.TabIndex = 3;
+            this.Calculatorlabel4.Text = "Second Number";
+            this.Calculatorlabel4.Click += new System.EventHandler(this.Calculatorlabel4_Click);
+            // 
+            // Calculatorentry2
+            // 
+            this.Calculatorentry2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorentry2.Location = new System.Drawing.Point(479, 172);
+            this.Calculatorentry2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorentry2.Multiline = true;
+            this.Calculatorentry2.Name = "Calculatorentry2";
+            this.Calculatorentry2.Size = new System.Drawing.Size(161, 80);
+            this.Calculatorentry2.TabIndex = 4;
+            this.Calculatorentry2.TextChanged += new System.EventHandler(this.Calculatorentry2_TextChanged);
+            // 
+            // Calculatorbutton1
+            // 
+            this.Calculatorbutton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton1.Location = new System.Drawing.Point(289, 81);
+            this.Calculatorbutton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorbutton1.Name = "Calculatorbutton1";
+            this.Calculatorbutton1.Size = new System.Drawing.Size(87, 36);
+            this.Calculatorbutton1.TabIndex = 5;
+            this.Calculatorbutton1.Text = "+";
+            this.Calculatorbutton1.UseVisualStyleBackColor = true;
+            this.Calculatorbutton1.Click += new System.EventHandler(this.Calculatorbutton1_Click);
+            // 
+            // Calculatorbutton2
+            // 
+            this.Calculatorbutton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton2.Location = new System.Drawing.Point(289, 132);
+            this.Calculatorbutton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorbutton2.Name = "Calculatorbutton2";
+            this.Calculatorbutton2.Size = new System.Drawing.Size(87, 36);
+            this.Calculatorbutton2.TabIndex = 6;
+            this.Calculatorbutton2.Text = "-";
+            this.Calculatorbutton2.UseVisualStyleBackColor = true;
+            this.Calculatorbutton2.Click += new System.EventHandler(this.Calculatorbutton2_Click);
+            // 
+            // Calculatorbuton3
+            // 
+            this.Calculatorbuton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbuton3.Location = new System.Drawing.Point(289, 187);
+            this.Calculatorbuton3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorbuton3.Name = "Calculatorbuton3";
+            this.Calculatorbuton3.Size = new System.Drawing.Size(87, 34);
+            this.Calculatorbuton3.TabIndex = 7;
+            this.Calculatorbuton3.Text = "x";
+            this.Calculatorbuton3.UseVisualStyleBackColor = true;
+            this.Calculatorbuton3.Click += new System.EventHandler(this.Calculatorbuton3_Click);
+            // 
+            // Calculatorbutton4
+            // 
+            this.Calculatorbutton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton4.Location = new System.Drawing.Point(289, 238);
+            this.Calculatorbutton4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorbutton4.Name = "Calculatorbutton4";
+            this.Calculatorbutton4.Size = new System.Drawing.Size(87, 37);
+            this.Calculatorbutton4.TabIndex = 8;
+            this.Calculatorbutton4.Text = "÷";
+            this.Calculatorbutton4.UseVisualStyleBackColor = true;
+            this.Calculatorbutton4.Click += new System.EventHandler(this.Calculatorbutton4_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Calculatorlabel5
+            // 
+            this.Calculatorlabel5.AutoSize = true;
+            this.Calculatorlabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorlabel5.Location = new System.Drawing.Point(739, 204);
+            this.Calculatorlabel5.Name = "Calculatorlabel5";
+            this.Calculatorlabel5.Size = new System.Drawing.Size(30, 31);
+            this.Calculatorlabel5.TabIndex = 10;
+            this.Calculatorlabel5.Text = "=";
+            this.Calculatorlabel5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Calculatorentry3
+            // 
+            this.Calculatorentry3.Enabled = false;
+            this.Calculatorentry3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorentry3.Location = new System.Drawing.Point(829, 172);
+            this.Calculatorentry3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorentry3.Multiline = true;
+            this.Calculatorentry3.Name = "Calculatorentry3";
+            this.Calculatorentry3.Size = new System.Drawing.Size(173, 80);
+            this.Calculatorentry3.TabIndex = 11;
+            this.Calculatorentry3.Text = "0";
+            this.Calculatorentry3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Calculatorbutton5
+            // 
+            this.Calculatorbutton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton5.Location = new System.Drawing.Point(289, 295);
+            this.Calculatorbutton5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Calculatorbutton5.Name = "Calculatorbutton5";
+            this.Calculatorbutton5.Size = new System.Drawing.Size(87, 34);
+            this.Calculatorbutton5.TabIndex = 12;
+            this.Calculatorbutton5.Text = "Mod";
+            this.Calculatorbutton5.UseVisualStyleBackColor = true;
+            this.Calculatorbutton5.Click += new System.EventHandler(this.Calculatorbutton5_Click);
+            // 
+            // Calculatorlabel6
+            // 
+            this.Calculatorlabel6.AutoSize = true;
+            this.Calculatorlabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorlabel6.Location = new System.Drawing.Point(874, 132);
+            this.Calculatorlabel6.Name = "Calculatorlabel6";
+            this.Calculatorlabel6.Size = new System.Drawing.Size(66, 25);
+            this.Calculatorlabel6.TabIndex = 13;
+            this.Calculatorlabel6.Text = "Result";
+            // 
+            // Calculatorbutton6
+            // 
+            this.Calculatorbutton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton6.Location = new System.Drawing.Point(289, 348);
+            this.Calculatorbutton6.Name = "Calculatorbutton6";
+            this.Calculatorbutton6.Size = new System.Drawing.Size(87, 34);
+            this.Calculatorbutton6.TabIndex = 14;
+            this.Calculatorbutton6.Text = "C";
+            this.Calculatorbutton6.UseVisualStyleBackColor = true;
+            this.Calculatorbutton6.Click += new System.EventHandler(this.Calculatorbutton6_Click);
+            // 
+            // Calculatorbutton7
+            // 
+            this.Calculatorbutton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton7.Location = new System.Drawing.Point(289, 401);
+            this.Calculatorbutton7.Name = "Calculatorbutton7";
+            this.Calculatorbutton7.Size = new System.Drawing.Size(87, 33);
+            this.Calculatorbutton7.TabIndex = 15;
+            this.Calculatorbutton7.Text = "x²";
+            this.Calculatorbutton7.UseVisualStyleBackColor = true;
+            this.Calculatorbutton7.Click += new System.EventHandler(this.Calculatorbutton7_Click);
+            // 
+            // Calculatorbutton8
+            // 
+            this.Calculatorbutton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton8.Location = new System.Drawing.Point(98, 293);
+            this.Calculatorbutton8.Name = "Calculatorbutton8";
+            this.Calculatorbutton8.Size = new System.Drawing.Size(87, 36);
+            this.Calculatorbutton8.TabIndex = 16;
+            this.Calculatorbutton8.Text = "DEL";
+            this.Calculatorbutton8.UseVisualStyleBackColor = true;
+            this.Calculatorbutton8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Calculatorbutton9
+            // 
+            this.Calculatorbutton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Calculatorbutton9.Location = new System.Drawing.Point(511, 293);
+            this.Calculatorbutton9.Name = "Calculatorbutton9";
+            this.Calculatorbutton9.Size = new System.Drawing.Size(87, 33);
+            this.Calculatorbutton9.TabIndex = 17;
+            this.Calculatorbutton9.Text = "DEL";
+            this.Calculatorbutton9.UseVisualStyleBackColor = true;
+            this.Calculatorbutton9.Click += new System.EventHandler(this.Calculatorbutton9_Click);
+            // 
+            // Calculatorlabel1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(1044, 528);
+            this.Controls.Add(this.Calculatorbutton9);
+            this.Controls.Add(this.Calculatorbutton8);
+            this.Controls.Add(this.Calculatorbutton7);
+            this.Controls.Add(this.Calculatorbutton6);
+            this.Controls.Add(this.Calculatorlabel6);
+            this.Controls.Add(this.Calculatorbutton5);
+            this.Controls.Add(this.Calculatorentry3);
+            this.Controls.Add(this.Calculatorlabel5);
+            this.Controls.Add(this.Calculatorbutton4);
+            this.Controls.Add(this.Calculatorbuton3);
+            this.Controls.Add(this.Calculatorbutton2);
+            this.Controls.Add(this.Calculatorbutton1);
+            this.Controls.Add(this.Calculatorentry2);
+            this.Controls.Add(this.Calculatorlabel4);
+            this.Controls.Add(this.Calculatorentry1);
+            this.Controls.Add(this.Calculatorlabel3);
+            this.Controls.Add(this.Calculatorlabel2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Calculatorlabel1";
+            this.Text = "Simple Calculator";
+            this.Load += new System.EventHandler(this.Calculatorlabel1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Calculatorlabel2;
+        private System.Windows.Forms.Label Calculatorlabel3;
+        private System.Windows.Forms.TextBox Calculatorentry1;
+        private System.Windows.Forms.Label Calculatorlabel4;
+        private System.Windows.Forms.TextBox Calculatorentry2;
+        private System.Windows.Forms.Button Calculatorbutton1;
+        private System.Windows.Forms.Button Calculatorbutton2;
+        private System.Windows.Forms.Button Calculatorbuton3;
+        private System.Windows.Forms.Button Calculatorbutton4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label Calculatorlabel5;
+        private System.Windows.Forms.TextBox Calculatorentry3;
+        private System.Windows.Forms.Button Calculatorbutton5;
+        private System.Windows.Forms.Label Calculatorlabel6;
+        private System.Windows.Forms.Button Calculatorbutton6;
+        private System.Windows.Forms.Button Calculatorbutton7;
+        private System.Windows.Forms.Button Calculatorbutton8;
+        private System.Windows.Forms.Button Calculatorbutton9;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculatorlabel1 : Form
+    {
+        public Calculatorlabel1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculatorlabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculatorlabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculatorbutton1_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" || Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter all numbers","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                int number_1 = Convert.ToInt32(Calculatorentry1.Text);
+                int number_2 = Convert.ToInt32(Calculatorentry2.Text);
+                int total = number_1 + number_2;
+                Calculatorentry3.Text = total.ToString();
+            }catch
+            {
+                MessageBox.Show("Please enter valid numbers", "Warninig", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+        }
+             
+            
+        
+
+        private void Calculatorbutton2_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" || Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter all numbers","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                int number_1 = Convert.ToInt32(Calculatorentry1.Text);
+                int number_2 = Convert.ToInt32(Calculatorentry2.Text);
+                int total = number_1 - number_2;
+                Calculatorentry3.Text = total.ToString();
+            }catch
+            {
+                MessageBox.Show("Please enter valid number", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+        }
+
+        private void Calculatorbuton3_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" || Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter all numbers","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                int number_1 = Convert.ToInt32(Calculatorentry1.Text);
+                int number_2 = Convert.ToInt32(Calculatorentry2.Text);
+                int total = number_1 * number_2;
+                Calculatorentry3.Text = total.ToString();
+            }catch
+            {
+                MessageBox.Show("Please enter valid numbers", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return ;
+            }
+            
+        }
+
+        private void Calculatorbutton4_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" || Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter all numbers","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                float number_1 = Convert.ToInt32(Calculatorentry1.Text);
+                float number_2 = Convert.ToInt32(Calculatorentry2.Text);
+                float total = number_1 / number_2;
+                Calculatorentry3.Text = total.ToString();
+            }catch
+            {
+                MessageBox.Show("Please enter valid numbers", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+        }
+
+        private void Calculatorbutton5_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" || Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter all numbers","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                int number_1 = Convert.ToInt32(Calculatorentry1.Text);
+                int number_2 = Convert.ToInt32(Calculatorentry2.Text);
+                int total = number_1 % number_2;
+                Calculatorentry3.Text = total.ToString();
+            }catch
+            {
+                MessageBox.Show("Please enter valid numbers","Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+            
+        }
+
+        private void Calculatorentry2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculatorbutton6_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" & Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter all number","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            Calculatorentry1.Text = "";
+            Calculatorentry2.Text = "";
+            Calculatorentry3.Text = "0";
+        }
+
+        private void Calculatorlabel1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculatorbutton7_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text == "" & Calculatorentry2.Text == "")
+            {
+                MessageBox.Show("Please enter the first number","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            if (Calculatorentry1.Text.Length > 0 & Calculatorentry2.Text.Length > 0)
+            {
+                MessageBox.Show("Please only enter the first number","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return ;
+            }
+            try
+            {
+                int number_1 = Convert.ToInt32(Calculatorentry1.Text);
+                int total = number_1 * number_1;
+                Calculatorentry3.Text = total.ToString();
+            }catch
+            {
+                MessageBox.Show("Please enter valid numbers", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry1.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter all number", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (Calculatorentry1.Text.Length > 0)
+            {
+                Calculatorentry1.Text = Calculatorentry1.Text.Substring(0, Calculatorentry1.Text.Length - 1);
+            }
+            
+        }
+
+        private void Calculatorbutton9_Click(object sender, EventArgs e)
+        {
+            if (Calculatorentry2.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter all number", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (Calculatorentry2.Text.Length > 0)
+            {
+                Calculatorentry2.Text = Calculatorentry2.Text.Substring(0, Calculatorentry2.Text.Length - 1);
+            }
+        }
+    }
+}
